9653bffd6b94da066a7ca2a4a560208c92fcc4d2
Patrick Assaf <patrickassaf96@gmail.com>
2020-04-18 11:42:29 -0400

    Connect elements to view controller outlets

--- tax-calculator/tax-calculator/ViewController.swift
+++ tax-calculator/tax-calculator/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var priceText: UITextField!
+    @IBOutlet weak var salesTaxText: UITextField!
+    @IBOutlet weak var totalPriceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateTotalPrice(_ sender: Any) {
+        
+    }
+    
 }
 
